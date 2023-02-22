a79ac1def2385ae68cb32febfa5ad8502e0ac6b7
kevin-riveros <riverosk885@gmail.com>
2023-02-22 06:13:59 -0500

    added meta tags

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -1,6 +1,7 @@
 import FooterComponent from "@/components/shared/Footer";
 import NavbarComponent from "@/components/shared/Navbar";
 import { Box } from "@chakra-ui/react";
+import Head from "next/head";
 import React, { useState } from "react";
 import About from "./About";
 import Blog from "./Blog";
@@ -11,6 +12,13 @@ import Projects from "./Projects";
 export default function HomePage() {
   return (
     <Box>
+      <Head>
+        <title>Kevin Riveros | Software Engineer | React.js Expert ⚛️</title>
+        <meta
+          name="description"
+          content="Hi there. I'm Kevin Riveros. Software Engineer based in Lima, Perú. Specializing in building exceptional, high-quality websites and applications."
+        />
+      </Head>
       <NavbarComponent />
       <Hero />
       <About />
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,6 +6,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
